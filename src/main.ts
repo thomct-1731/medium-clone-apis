509da4bdb95fa4d1fbab7caced4b8b48406f789b
thomct-1731 <chu.thi.thom@sun-asterisk.com>
2025-09-16 10:08:34 +0700

    fix: Fix comment - change process.env.X to using the config module

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { VersioningType } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const config = app.get(ConfigService);
+
   // Enable API versioning
   app.enableVersioning({
     type: VersioningType.URI,
-    defaultVersion: process.env.DEFAULT_VERSION || '1',
+    defaultVersion: config.get<string>('DEFAULT_VERSION') || '1',
   });
 
   // Swagger setup
-  const config = new DocumentBuilder()
-    .setTitle(process.env.SWAGGER_TITLE || 'API Documentation')
-    .setDescription(process.env.SWAGGER_DESCRIPTION || 'API Description')
-    .setVersion(process.env.SWAGGER_VERSION || '1.0')
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle(config.get<string>('SWAGGER_TITLE') || 'API Documentation')
+    .setDescription(
+      config.get<string>('SWAGGER_DESCRIPTION') || 'API Description',
+    )
+    .setVersion(config.get<string>('SWAGGER_VERSION') || '1.0')
     .addBearerAuth() // JWT
     .build();
-  const document = SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api/docs', app, document);
 
-  await app.listen(process.env.APP_PORT || 3000);
+  await app.listen(config.get<number>('APP_PORT') || 3000);
 }
 bootstrap();
