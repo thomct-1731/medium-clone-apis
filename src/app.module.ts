4faeca4e644e903d6f0df49d62c590a54888e9de
thomct-1731 <chu.thi.thom@sun-asterisk.com>
2025-09-19 15:58:24 +0700

    [BE] [Base] Config i18n

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
+import { CustomI18nModule } from './i18n/i18n.module';
 import { UsersModule } from './users/users.module';
 import { UserTokensModule } from './user-tokens/user-tokens.module';
 import databaseConfig from './config/database.config';
@@ -15,6 +16,7 @@ import databaseConfig from './config/database.config';
       load: [databaseConfig],
     }),
     DatabaseModule,
+    CustomI18nModule,
     UsersModule,
     UserTokensModule,
   ],
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { I18nService, I18nContext } from 'nestjs-i18n';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private readonly logger = new Logger(AppService.name);
+
+  constructor(private readonly i18n: I18nService) {}
+
+  getHello(i18n?: I18nContext): string {
+    const lang = i18n?.lang || 'en';
+    this.logger.log(`Current language: ${lang}`);
+
+    return this.i18n.t('common.HELLO', { lang });
   }
 }
